fb42fb12248dd0c048d3f019a8fae15289864389
karan suthar <karan.suthar.lakha@gmail.com>
2026-04-03 01:10:46 +0530

    feat: rename backend class to Main and restructure application entry point

--- backend/src/main/java/solutions/triniti/backend/Main.java
+++ backend/src/main/java/solutions/triniti/backend/Main.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class backend {
+public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(backend.class,args);
+        SpringApplication.run(Main.class, args);
     }
 }
